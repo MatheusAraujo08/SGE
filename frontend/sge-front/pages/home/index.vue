5a5ce1056462d7a8beb19f12c8707c2ed84e631e
duduaugusto123 <123705813+duduaugusto123@users.noreply.github.com>
2024-03-21 20:41:39 -0300

    aula

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+definePageMeta({
+    middleware: 'auth'
+})
+
+
+const {signOut} = useAuth();
+
+const Logout = () => {
+    console.log('Fazendo Logout');
+
+    signOut({redirect:false}).then(()=> {
+    console.log('Logout com Sucesso');
+    navigateTo('/')})
+
+    .catch((error)=> {
+        console.log('deu algum erro');
+    });
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1>   
+        <button  type='submit' class="customButton" @click="Logout">SAIR</button> 
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
